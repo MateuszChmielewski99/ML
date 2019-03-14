dab1818c3888b71c4955a7a30c41d0a1281c229e
Mateusz Chmielewski <45406503+MateuszChmielewski99@users.noreply.github.com>
2019-03-14 18:27:10 +0100

    Add files via upload
    Simple linear regression written in C#

---
+++ LinearRegression.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class LinearRegression<T> where T : struct
+    {
+        private Dictionary<T,T> data;
+        private double a;
+        private double b;
+
+        public LinearRegression(Dictionary<T, T> data)
+        {
+            this.data = data;
+        }
+
+        private void train()
+        {
+            dynamic AvgX = data.Average(s=>(dynamic)s.Key);
+            dynamic AvgY = data.Average(s => (dynamic)s.Value);
+            var tmpL = 0.0;
+            var tmpM = 0.0;
+
+            foreach (KeyValuePair<T, T> cur in data)
+            {
+                tmpL += cur.Value * (cur.Key - AvgX);
+                tmpM += Math.Pow(cur.Key - AvgX, 2);
+            }
+            this.a = tmpL/tmpM;
+            this.b = AvgY - AvgX * this.a;
+        }
+
+        public void Test (Dictionary<T,T> testData)
+        {
+            foreach (KeyValuePair<T, T> currnet in testData)
+                Console.WriteLine("Value: {0} Value predicted : {1}", currnet.Value,Predict(currnet.Key));
+        }
+
+        public T Predict(T dataToPredict)
+        {
+            train();
+            dynamic dataPr = dataToPredict;
+            return (T)(this.a * dataPr + this.b);
+        }
+
+        public override string ToString()
+        {
+            return "y= "+this.a + " x + " + this.b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,int> data = new Dictionary<int, int>{ {210,140},{ 270,190},{290,250 },
+                {310,270}, { 450, 340},{480, 360},{ 510, 420 }, { 520, 390 }};
+
+            Dictionary<int, int> testData = new Dictionary<int, int> { { 370, 290 }, { 400, 310 } };
+            LinearRegression<int> linearRegression = new LinearRegression<int>(data);
+            linearRegression.Test(testData);
+
+            Console.ReadKey();
+        }
+    }
+}
